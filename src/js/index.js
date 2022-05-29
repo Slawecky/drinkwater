5d05fd5bf300b689afe62ef761f9d19f5cf87eab
Slawecky <comsite@comsite.com.pl>
2022-05-29 16:32:59 +0200

    Batony

--- src/js/index.js
+++ src/js/index.js
@@ -6,4 +6,35 @@ import '../scss/main.scss';
 
 /* place your code below */
 
-console.log('HELLO 🚀')
+const countCounter = document.querySelector('.count_counter--js');
+const buttonAdd = document.querySelector('.button__add--js');
+const buttonRemove = document.querySelector(".button_remove--js");
+const key = new Date().toLocaleString().slice(0,10);
+const localStorageValue = localStorage.getItem(key);
+
+console.log(key);
+
+let currentGlassCounter = 0;
+
+if(localStorageValue) {
+    currentGlassCounter = localStorageValue;
+}
+else {
+    localStorage.setItem(key,0);
+}
+
+countCounter.innerHTML = currentGlassCounter;
+
+buttonAdd.addEventListener('click', () => {
+    currentGlassCounter++;
+    countCounter.innerHTML = currentGlassCounter;
+    localStorage.setItem(key, currentGlassCounter);
+})
+
+buttonRemove.addEventListener("click", () => {
+  if (currentGlassCounter > 0) { 
+      currentGlassCounter--;
+      }
+  countCounter.innerHTML = currentGlassCounter;
+  localStorage.setItem(key, currentGlassCounter);
+});
